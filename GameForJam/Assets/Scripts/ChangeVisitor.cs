ba080d378004ed01b828365b3f05187341ed8680
just-another-man <a.sharikov.work@gmail.com>
2022-04-03 16:30:11 +0500

    script and BrewedPotion

--- GameForJam/Assets/Scripts/ChangeVisitor.cs
+++ GameForJam/Assets/Scripts/ChangeVisitor.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeVisitor : MonoBehaviour
+namespace DefaultNamespace
 {
-   public static ChangeVisitor instance;
+public class ChangeVisitor : MonoBehaviour
+   {
+        public static ChangeVisitor instance;
 
-        private void Awake()
-        {
-            if (instance is null)
-                instance = this;
-            else
+            private void Awake()
             {
-                Debug.LogError("double singleton:"+this.GetType().Name);
+                if (instance is null)
+                    instance = this;
+                else   
+                {
+                    Debug.LogError("double singleton:"+this.GetType().Name);
+                }
+            }    
+            public void Enter(Villager villager)
+            {
+            
             }
-            
-        public void Enter(Villager villager)
-        {
-            
-        }
+    }
 }

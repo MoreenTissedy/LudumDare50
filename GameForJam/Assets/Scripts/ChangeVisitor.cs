146b603155719a2b2e57f4eaf613037451deec5a
just-another-man <a.sharikov.work@gmail.com>
2022-04-03 16:20:34 +0500

    Clear script ChangeVisitor

---
+++ GameForJam/Assets/Scripts/ChangeVisitor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeVisitor : MonoBehaviour
+{
+   public static ChangeVisitor instance;
+
+        private void Awake()
+        {
+            if (instance is null)
+                instance = this;
+            else
+            {
+                Debug.LogError("double singleton:"+this.GetType().Name);
+            }
+            
+        public void Enter(Villager villager)
+        {
+            
+        }
+}

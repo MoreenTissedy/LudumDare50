b075f8dd2db3d0020f3d60b7d710e2a3152ab711
MoreenTissedy <veronikabyzova@gmail.com>
2022-04-17 23:37:40 +0500

    new UI final changes

---
+++ GameForJam/Assets/Scripts/DisplayNumberedScale.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class DisplayNumberedScale : MonoBehaviour
+    {
+        private Text text;
+
+        private void Start()
+        {
+            text = GetComponent<Text>();
+            UpdateMoney();
+            GameManager.instance.money.changed += UpdateMoney;
+        }
+
+        private void UpdateMoney()
+        {
+            text.text = $"{GameManager.instance.money.Value()} / {GameManager.instance.statusBarsMax}";
+        }
+    
+    }
+}

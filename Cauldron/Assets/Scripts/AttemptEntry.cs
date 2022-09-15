3f60b7bcb369339d39e426f86d1280c73b1f367c
just-another-man <a.sharikov.work@gmail.com>
2022-09-15 13:29:09 +0500

    Few small changes
    1) Fixed attempts in the book
    2) Fixed buttons (to next and prev pages
    3) Changes in the night panel

--- Cauldron/Assets/Scripts/AttemptEntry.cs
+++ Cauldron/Assets/Scripts/AttemptEntry.cs
@@ -23,7 +23,7 @@ namespace CauldronCodebase
                 Debug.LogError("failed attempts are recorded as an array of 3 ingredients");
                 return;
             }
-
+            
             image1.sprite = data.Get(attempt[0]).image;
             image2.sprite = data.Get(attempt[1]).image;
             image3.sprite = data.Get(attempt[2]).image;

d1690f753b067baf5996059c57fe0306c5f5250c
MoreenTissedy <veronikabyzova@gmail.com>
2022-10-23 17:16:35 +0500

    Book button tweaks

--- Cauldron/Assets/Scripts/UI/RecipeBookChangeModeButton.cs
+++ Cauldron/Assets/Scripts/UI/RecipeBookChangeModeButton.cs
@@ -1,18 +1,42 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Zenject;
 
 namespace CauldronCodebase
 {
-    public class RecipeBookChangeModeButton : MonoBehaviour, IPointerClickHandler
+    public class RecipeBookChangeModeButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
         public RecipeBook.Mode mode;
+        public Transform hint;
+        private float hintAnimDuration = 0.3f;
         [Inject]
         private RecipeBook recipeBook;
-        
+
+        private float localScaleX;
+
+        private void Awake()
+        {
+            var localScale = hint.localScale;
+            localScaleX = localScale.x;
+            localScale = new Vector3(0, localScale.y, localScale.z);
+            hint.localScale = localScale;
+        }
+
         public void OnPointerClick(PointerEventData eventData)
         {
             recipeBook.ChangeMode(mode);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            hint.transform.DOScaleX(localScaleX, hintAnimDuration).From(0);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            hint.transform.DOScaleX(0, hintAnimDuration);
         }
     }
 }

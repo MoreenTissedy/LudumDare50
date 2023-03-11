f7890c4eaa3ee4dc3409c5051e92ed6a02891472
Dove0fPeace <lebedev07.98@gmail.com>
2023-03-11 16:05:44 +0500

    Fix crit

--- Cauldron/Assets/Scripts/UI/TimeBar.cs
+++ Cauldron/Assets/Scripts/UI/TimeBar.cs
@@ -21,71 +21,85 @@ namespace CauldronCodebase
         private MainSettings settings;
         private GameStateMachine gameStateMachine;
         private GameDataHandler gameDataHandler;
+        private NightPanel nightPanel;
 
+        private int currentDay;
         [Inject]
         private void Construct(MainSettings settings,
                                GameStateMachine gameStateMachine,
-                               GameDataHandler gameDataHandler)
+                               GameDataHandler gameDataHandler,
+                               NightPanel nightPanel)
         {
             this.settings = settings;
             this.gameStateMachine = gameStateMachine;
             this.gameDataHandler = gameDataHandler;
+            this.nightPanel = nightPanel;
         }
 
         private void Awake()
         {
             rectWidth = fullCycleSample.rect.width;
+            currentDay = gameDataHandler.currentDay + 1;
             
-            dayNumber.text = $"{dayText} 1";
-            
-            timeBar.anchoredPosition = new Vector2(-rectWidth / 2, 0);
+            NewDayReset(currentDay);
         }
 
         private void Start()
         {
             step = rectWidth/(settings.gameplay.cardsPerDay+3);
             
+            int startSteps = 2 + gameDataHandler.cardsDrawnToday;
+            ShiftBarOnLoad(startSteps);
+
             gameStateMachine.OnChangeState += OnNewDay;
             gameStateMachine.OnChangeState += OnNewVisitor;
+            nightPanel.OnClose += NewDayShift;
         }
 
         private void OnDestroy()
         {
             gameStateMachine.OnChangeState -= OnNewDay;
             gameStateMachine.OnChangeState -= OnNewVisitor;
+            nightPanel.OnClose -= NewDayShift;
         }
 
         private void OnNewVisitor(GameStateMachine.GamePhase phase)
         {
             if (phase != GameStateMachine.GamePhase.Visitor) return;
-            float newStep = step;
-            //longer shift after night
-            if (gameDataHandler.cardsDrawnToday == 0)
-                newStep = step * 2;
-            timeBar.DOLocalMoveX(timeBar.anchoredPosition.x-newStep, speed);
+
+            if(gameDataHandler.cardsDrawnToday == 0) return;
+            timeBar.DOLocalMoveX(timeBar.anchoredPosition.x-step, speed);
         }
 
         private void OnNewDay(GameStateMachine.GamePhase phase)
         {
             if (phase != GameStateMachine.GamePhase.Night) return;
-            //int day = gameDataHandler.currentDay;
+            currentDay++;
+
             //longer shift before night
-            if (gameDataHandler == null || gameDataHandler.currentDay == null)
-            {
-                Debug.Log("Troubles with game data handler");
-                return;
-            }
             timeBar.DOLocalMoveX(timeBar.anchoredPosition.x - step*2, speed).
                 SetEase(Ease.InOutSine).
-                OnComplete(() => NewDayReset(gameDataHandler.currentDay));
+                OnComplete(() => NewDayReset(currentDay));
         }
 
         void NewDayReset(int day)
         {
             
-            dayNumber.text = $"{dayText} {day + 1}";
+            dayNumber.text = $"{dayText} {day}";
             timeBar.anchoredPosition = new Vector2(-rectWidth / 2, 0);
         }
 
+        private void NewDayShift()
+        {
+            timeBar.DOLocalMoveX(timeBar.anchoredPosition.x - step * 2, speed).SetEase(Ease.InOutSine);
+        }
+
+        private void ShiftBarOnLoad(int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                timeBar.anchoredPosition = new Vector2(timeBar.anchoredPosition.x - step, 0);
+            }
+        }
     }
 }

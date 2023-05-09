f407e5551037402754fa2905a8b8689608e16eb4
Dove0fPeace <lebedev07.98@gmail.com>
2023-05-09 15:55:43 +0500

    Fade text

---
+++ Cauldron/Assets/Scripts/UI/DayStageFX.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class DayStageFX : MonoBehaviour
+{
+    [SerializeField] private TMP_Text tmpTextComponent;
+    [SerializeField] private string text;
+    [SerializeField] private CanvasGroup canvasGroup;
+    public float duration;
+
+    private void Start()
+    {
+        tmpTextComponent.text = text;
+
+        canvasGroup.DOFade(1, duration).SetLoops(2, LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+}

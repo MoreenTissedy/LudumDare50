465b30379cea65ddae218689b326b9dcaccd1fdb
Dr_Wat <hudorojckov.leva@gmail.com>
2023-06-06 11:47:50 +0500

    FIX button from inspector

---
+++ Cauldron/Assets/Scripts/UI/Menu/AuthorsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Cysharp.Threading.Tasks.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CauldronCodebase
+{
+    public class AuthorsMenu : MonoBehaviour
+    {
+        [SerializeField] private Button closePanelButton;
+
+        private void Start()
+        {
+            closePanelButton.onClick.AddListener(Close);
+        }
+
+        public void Open()
+        {
+            gameObject.SetActive(true);
+        }
+        
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

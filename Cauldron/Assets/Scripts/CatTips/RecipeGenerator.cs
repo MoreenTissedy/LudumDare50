d028c16092584659f1fcb8ef78a6ac95da510f85
Dove0fPeace <lebedev07.98@gmail.com>
2023-09-15 15:33:29 +0500

    Generating the correct recipe with some chance

---
+++ Cauldron/Assets/Scripts/CatTips/RecipeGenerator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Random = System.Random;
+
+namespace CauldronCodebase
+{
+    public static class RecipeGenerator
+    {
+        public static Ingredients[] GenerateRandomRecipe(RecipeBook book)
+        {
+            Ingredients[] randomRecipe;
+            var generatedRecipe = new List<Ingredients[]>(RecipeBook.MAX_COMBINATIONS_COUNT);
+            int tryCount = 0;
+
+            do
+            {
+                randomRecipe = GenerateIngredients();
+
+                if (generatedRecipe.Any(recipe => recipe.All(randomRecipe.Contains)))
+                {
+                    continue;
+                }
+
+                tryCount++;
+                generatedRecipe.Add(randomRecipe);
+
+                if (tryCount >= RecipeBook.MAX_COMBINATIONS_COUNT)
+                {
+                    return null;
+                }
+            
+            } while (book.IsIngredientSetKnown(randomRecipe));
+            
+            return randomRecipe;
+        }
+
+        public static Recipe GenerateLockedRecipe(RecipeBook book)
+        {
+            return book.LockedRecipe.Count == 0 ? null : book.LockedRecipe[UnityEngine.Random.Range(0, book.LockedRecipe.Count)];
+        }
+
+        public static Ingredients[] GenerateLastIngredientRecipe(Ingredients[] mix, RecipeBook book)
+        {
+            Ingredients[] recipeToTips;
+            Ingredients randomIngredient;
+
+            List<Ingredients> allIngredients = Enum.GetValues(typeof(Ingredients)).Cast<Ingredients>().ToList();
+            foreach (var ingredientInMix in mix)
+            {
+                allIngredients.Remove(ingredientInMix);
+            }
+
+            int tryCount = allIngredients.Count;
+            do
+            {
+                randomIngredient = allIngredients[UnityEngine.Random.Range(0, allIngredients.Count)];
+                recipeToTips = new[] {mix[0], mix[1], randomIngredient};
+
+                tryCount -= 1;
+                if (tryCount <= 0) break;
+            } while (book.IsIngredientSetKnown(recipeToTips));
+
+            return tryCount > 0 ? recipeToTips : null;
+        }
+
+        public static Ingredients[] GenerateCorrectLastIngredientRecipe(Ingredients[] mix, RecipeBook book)
+        {
+            List<Recipe> potentialRecipes = new List<Recipe>();
+
+            foreach (var recipe in book.LockedRecipe)
+            {
+                if(mix.All(recipe.RecipeIngredients.Contains))
+                {
+                    potentialRecipes.Add(recipe);
+                }
+            }
+
+            return potentialRecipes[UnityEngine.Random.Range(0, potentialRecipes.Count)].RecipeIngredients.ToArray();
+        }
+
+        private static Ingredients[] GenerateIngredients()
+        {
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+        
+            var ingredients = Enum.GetValues(typeof(Ingredients)).Cast<Ingredients>().ToList();
+
+            return ingredients.OrderBy(x => rnd.Next()).Take(3).ToArray();
+        }
+    }
+}
--- Cauldron/Assets/Scripts/CatTips/TipsCallers/WaitSlowTips.cs
+++ Cauldron/Assets/Scripts/CatTips/TipsCallers/WaitSlowTips.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using Zenject;
-using Random = System.Random;
 
 namespace CauldronCodebase.CatTips
 {
@@ -31,40 +28,20 @@ namespace CauldronCodebase.CatTips
             }
 
             Ingredients[] randomRecipe;
-            List<Ingredients[]> generatedRecipe = new List<Ingredients[]>(RecipeBook.MAX_COMBINATIONS_COUNT);
-            int tryCount = 0;
-
-            do
+            if (gameDataHandler.wrongPotionsCount >= WrongPotionThreshold && Random.Range(0, 100) <= ChanceToUnlock)
             {
-                randomRecipe = GenerateRandomRecipe();
-
-                if (generatedRecipe.Any(recipe => recipe.All(randomRecipe.Contains)))
-                {
-                    continue;
-                }
-
-                tryCount++;
-                generatedRecipe.Add(randomRecipe);
-
-                if (tryCount >= RecipeBook.MAX_COMBINATIONS_COUNT)
-                {
-                    yield break;
-                }
+                randomRecipe = RecipeGenerator.GenerateLockedRecipe(recipeBook).RecipeIngredients.ToArray();
+            }
+            else
+            {
+                randomRecipe = RecipeGenerator.GenerateRandomRecipe(recipeBook);
+            }
             
-            } while (recipeBook.IsIngredientSetKnown(randomRecipe));
+            if (randomRecipe == null) yield break;
             
             var ingredients = randomRecipe.Select(ingredient => ingredientsData.Get(ingredient)).ToList();
         
             CatTipsValidator.ShowTips(CatTipsGenerator.CreateTipsWithIngredients(catTipsProvider.SlowPlayerTips, ingredients));
-        }
-
-        private Ingredients[] GenerateRandomRecipe()
-        {
-            var rnd = new Random(Guid.NewGuid().GetHashCode());
-        
-            var ingredients = Enum.GetValues(typeof(Ingredients)).Cast<Ingredients>().ToList();
-
-            return ingredients.OrderBy(x => rnd.Next()).Take(3).ToArray();
 
         }
     }

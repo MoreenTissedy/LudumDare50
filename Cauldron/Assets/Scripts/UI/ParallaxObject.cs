a28d72d55da7826e02ec4f3594d93325b2faa54f
Dove0fPeace <lebedev07.98@gmail.com>
2023-11-15 23:01:06 +0300

    Add lerp to parallax

--- Cauldron/Assets/Scripts/UI/ParallaxObject.cs
+++ Cauldron/Assets/Scripts/UI/ParallaxObject.cs
@@ -4,7 +4,8 @@ public class ParallaxObject : MonoBehaviour
 {
     private const float PARALLAX_VERTICAL_POWER_MODIFIER = 0.3F;
     [SerializeField] private float parallaxPower;
-    private Vector3 StartPosition;
+    private float parallaxSpeed = 1.3f;
+    private Vector2 StartPosition;
 
     private void Start()
     {
@@ -13,10 +14,11 @@ public class ParallaxObject : MonoBehaviour
 
     private void LateUpdate()
     {
-        float x, y;
-        x = (Input.mousePosition.x - Screen.width / 2) * parallaxPower / Screen.width;
-        y = (Input.mousePosition.y - Screen.height / 2) * parallaxPower * PARALLAX_VERTICAL_POWER_MODIFIER / Screen.height;
-        
-        transform.position = StartPosition + new Vector3(x, y, 0);
+        float x = (Input.mousePosition.x - Screen.width / 2) * parallaxPower / Screen.width;
+        float y = (Input.mousePosition.y - Screen.height / 2) * parallaxPower * PARALLAX_VERTICAL_POWER_MODIFIER / Screen.height;
+
+        transform.position =  Vector2.Lerp(transform.position,
+                                                          StartPosition + new Vector2(x, y),
+                                                          parallaxSpeed * Time.unscaledDeltaTime);
     }
 }

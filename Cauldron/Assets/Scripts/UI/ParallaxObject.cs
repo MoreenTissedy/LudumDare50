fbb5d1f2c5185f1211351626b9da9841731e1bbd
Dove0fPeace <91125772+Dove0fPeace@users.noreply.github.com>
2023-11-19 15:31:57 +0300

    Improved parallax
    * Add lerp to parallax
    
    * Add delta between current and target position

--- Cauldron/Assets/Scripts/UI/ParallaxObject.cs
+++ Cauldron/Assets/Scripts/UI/ParallaxObject.cs
@@ -4,7 +4,8 @@ public class ParallaxObject : MonoBehaviour
 {
     private const float PARALLAX_VERTICAL_POWER_MODIFIER = 0.3F;
     [SerializeField] private float parallaxPower;
-    private Vector3 StartPosition;
+    private float parallaxSpeed = 15f;
+    private Vector2 StartPosition;
 
     private void Start()
     {
@@ -13,10 +14,14 @@ public class ParallaxObject : MonoBehaviour
 
     private void LateUpdate()
     {
-        float x, y;
-        x = (Input.mousePosition.x - Screen.width / 2) * parallaxPower / Screen.width;
-        y = (Input.mousePosition.y - Screen.height / 2) * parallaxPower * PARALLAX_VERTICAL_POWER_MODIFIER / Screen.height;
-        
-        transform.position = StartPosition + new Vector3(x, y, 0);
+        float x = (Input.mousePosition.x - Screen.width / 2) * parallaxPower / Screen.width;
+        float y = (Input.mousePosition.y - Screen.height / 2) * parallaxPower * PARALLAX_VERTICAL_POWER_MODIFIER / Screen.height;
+
+        Vector2 targetPosition = StartPosition + new Vector2(x, y);
+        float delta = (targetPosition - (Vector2)transform.position).magnitude;
+
+        transform.position =  Vector2.Lerp(transform.position,
+                                                          targetPosition,
+                                                          parallaxSpeed * delta * Time.unscaledDeltaTime);
     }
 }

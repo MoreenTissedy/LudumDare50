fe4e7e6741916038bf5d9d05da31b61ea6e819a1
MoreenTissedy <veronikabyzova@gmail.com>
2024-01-28 16:19:19 +0500

    Minor refactoring

--- Cauldron/Assets/Scripts/ExperimentController.cs
+++ Cauldron/Assets/Scripts/ExperimentController.cs
@@ -5,47 +5,6 @@ using CauldronCodebase;
 using NaughtyAttributes;
 using UnityEngine;
 
-public class IngredientSet: IEquatable<IngredientSet>, IComparable<IngredientSet>
-{
-    public IngredientSetType SetType;
-    public Recipe Recipe;
-    public Ingredients[] Ingredients = new Ingredients[3];
-
-    public bool Equals(IngredientSet other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-        foreach (Ingredients type in Ingredients)
-        {
-            if (!other.Ingredients.Contains(type))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public int CompareTo(IngredientSet other)
-    {
-        int typeComparison = SetType.CompareTo(other.SetType);
-        if (typeComparison != 0)
-        {
-            return typeComparison;
-        }
-        for (int i = 0; i < 3; i++)
-        {
-            var ingredientComparison = Ingredients[i].CompareTo(other.Ingredients[i]);
-            if (ingredientComparison != 0)
-            {
-                return ingredientComparison;
-            }
-        }
-        return 0;
-    }
-}
-
 public enum IngredientSetType
 {
     Unknown,

36d42055f493e4f9f64cd1a2ae4b0d2e38d38b06
klosyandr <klosyandr@gmail.com>
2024-09-02 01:06:07 +0800

    tiny fix for inspector

--- Cauldron/Assets/Scripts/InputCheatManager.cs
+++ Cauldron/Assets/Scripts/InputCheatManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -14,7 +15,7 @@ public class CheatCodes
 
 public class InputCheatManager : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] [ReadOnly]
     private string compositeString;
 
     [SerializeField,

2cc9aa849f0592f2bf03ed6db33b0acabcb68f63
MoreenTissedy <veronikabyzova@gmail.com>
2024-09-22 13:43:26 +0500

    Status override - fixed stack overflow

--- Cauldron/Assets/Scripts/GameModes/StatusOverrideGameMode.cs
+++ Cauldron/Assets/Scripts/GameModes/StatusOverrideGameMode.cs
@@ -1,3 +1,4 @@
+using CauldronCodebase.GameStates;
 using UnityEngine;
 using Zenject;
 
@@ -12,34 +13,49 @@ namespace CauldronCodebase
         public int Bonus = 5;
 
         private GameDataHandler gameDataHandler;
+        private GameStateMachine gameStateMachine;
+
+        private bool overriden;
 
         [Inject]
-        public void Construct(GameDataHandler gameDataHandler)
+        public void Construct(GameDataHandler gameDataHandler, GameStateMachine gameStateMachine)
         {
             this.gameDataHandler = gameDataHandler;
+            this.gameStateMachine = gameStateMachine;
         }
         public override void Apply()
         {
-            gameDataHandler.StatusChanged += CheckStatus;
+            gameStateMachine.OnChangeState += ResetAppliedFlag;
+            gameDataHandler.StatusChanged += TryOverrideStatus;
         }
 
-        private void CheckStatus(Statustype type, int diff)
+        private void ResetAppliedFlag(GameStateMachine.GamePhase phase)
         {
-            if (type != Status || diff == 0)
+            if (phase == GameStateMachine.GamePhase.Visitor)
+            {
+                overriden = false;
+            }
+        }
+
+        private void TryOverrideStatus(Statustype type, int diff)
+        {
+            if (overriden || type != Status || diff == 0)
             {
                 return;
             }
 
             if (diff > 0 && Up)
             {
+                overriden = true;
                 gameDataHandler.Add(type, GetBonus(diff));
-                Debug.LogError("+ "+GetBonus(diff));
+                Debug.Log(type + " + "+GetBonus(diff));
             }
 
             if (diff < 0 && Down)
             {
+                overriden = true;
                 gameDataHandler.Add(type, -GetBonus(-diff));
-                Debug.LogError("- "+GetBonus(-diff));
+                Debug.Log(type + " - "+GetBonus(-diff));
             }
         }
 

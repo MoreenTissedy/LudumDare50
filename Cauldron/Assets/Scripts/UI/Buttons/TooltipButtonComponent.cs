e60b7d34070cbdfff47140b059b11ce0c94244ef
klosyandr <klosyandr@gmail.com>
2025-02-20 14:45:35 +0800

    add TooltipButtonComponent

---
+++ Cauldron/Assets/Scripts/UI/Buttons/TooltipButtonComponent.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Universal
+{
+    public class TooltipButtonComponent: IAnimatedButtonComponent
+    {
+        [SerializeField] private ScrollTooltip tooltip;
+
+        public override void Activate(){}
+
+        public override void ChangeInteractive(bool isInteractive){}
+
+        public override void Select()
+        {
+            tooltip.Open();
+        }
+
+        public override void Unselect()
+        {
+            tooltip.Close();
+        }
+    }
+}
